ef91c88e846a08c0c333f3eefb6a71731da81f8f
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-01-23 22:41:57 +0530

    Initialization of backend and database connection.

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const connectToDb = require('./config/database')
+
+const userRoutes = require('./routes/user')
+const captainRoutes = require('./routes/captain')
+const mapsRoutes = require('./routes/maps.routes')
+const rideRoutes = require('./routes/ride.routes')
+
+connectToDb();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to default route");
+// })
+
+// Define Routes
+app.use('/users', userRoutes)
+app.use('/captains', captainRoutes)
+app.use('/maps', mapsRoutes)
+app.use('/rides', rideRoutes)
+
+module.exports = app;
---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const app = require('./app');
+
+const express = require('express');
+const path = require('path');
+
+const { initializeSocket } = require('./socket')
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+initializeSocket(server);
+
+
+// ----------- Deployment ----------
+
+const __dirname1 = path.resolve();
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, "../Frontend/dist")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "../Frontend", "dist", "index.html"))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running successfully.')
+    })
+}
+
+// ----------- Deployment ----------
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port no ${PORT}`);
+})
