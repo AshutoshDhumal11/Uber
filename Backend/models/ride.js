05065f9f13f0947f9f3cdbe88d333c83117e65e5
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-01-23 22:43:26 +0530

    Define all the models.

---
+++ Backend/models/ride.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+
+const rideSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    captain: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Captain',
+    },
+    pickup: {
+        type: String,
+        required: true,
+    },
+    destination: {
+        type: String,
+        required: true,
+    },
+    fare: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "accepted", "ongoing", "cancelled", "completed"],
+        default: "pending",
+    },
+    duration: {
+        type: Number, // in Seconds 
+    },
+    distance: {
+        type: Number, // in Minutes
+    },
+    paymentId: {
+        type: String,
+    },
+    orderId: {
+        type: String,
+    },
+    signature: {
+        type: String,
+    },
+    otp: {
+        type: String,
+        required: true,
+        select: false,
+    }
+})
+
+const RideModel = mongoose.model("Ride", rideSchema);
+module.exports = RideModel;
