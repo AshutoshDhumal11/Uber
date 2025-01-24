f09ffb37cdfda60430f81f9ad5f979bc2a9aafa7
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-01-24 12:37:03 +0530

    Final Changes done.

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const { initializeSocket } = require('./socket')
 const PORT = process.env.PORT || 3000;
@@ -12,23 +12,23 @@ const server = http.createServer(app);
 initializeSocket(server);
 
 
-// ----------- Deployment ----------
+// // ----------- Deployment ----------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, "../Frontend/dist")));
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname1, "../Frontend/dist")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "../Frontend", "dist", "index.html"))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running successfully.')
-    })
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, "../Frontend", "dist", "index.html"))
+//     })
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('API is running successfully.')
+//     })
+// }
 
-// ----------- Deployment ----------
+// // ----------- Deployment ----------
 
 server.listen(PORT, () => {
     console.log(`Server is running on port no ${PORT}`);
